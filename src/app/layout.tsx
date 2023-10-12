462bed8c93d32c9dd170f788705e886fc82352d9
Rely Ky <relyky@hotmail.com>
2023-10-12 12:25:21 +0800

    Todo List

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="zh-Hant">
+      <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>{children}</body>
     </html>
   )
 }
