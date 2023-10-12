8bfc8a93970296e85d175eb72c5749c7e5c1f315
Rely Ky <relyky@hotmail.com>
2023-10-12 14:56:43 +0800

    date-fns

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,25 +1,29 @@
 "use client"
+import { format } from 'date-fns'
 
 type TodoItemProps = {
     id: string
     title: string
     complete: boolean
+    createdAt: Date
+    updatedAt: Date
     toggleTodo: (id: string, complete: boolean) => void
 }
 
-export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+export function TodoItem({ id, title, complete, createdAt, updatedAt, toggleTodo }: TodoItemProps) {
     return (
-        <li className="flex gap-1 items-center">
-            <input
-                id={id}
-                type="checkbox"
-                className="cursor-pointer peer"
-                defaultChecked={complete}
-                onChange={e => toggleTodo(id, e.target.checked)}
-            />
-            <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
+        <li className="flex gap-1 justify-between items-center">
+            <label className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
+                <input
+                    id={id}
+                    type="checkbox"
+                    className="cursor-pointer peer"
+                    defaultChecked={complete}
+                    onChange={e => toggleTodo(id, e.target.checked)}
+                />
                 {title}
             </label>
+            <span className="font-mono">{format(createdAt, "yyyy-MM-dd HH:mm:ss")} | {format(updatedAt, "HH:mm:ss")}</span>
         </li>
     )
 }
